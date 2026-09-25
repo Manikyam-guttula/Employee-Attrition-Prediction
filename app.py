2b64814c82377573fabd565da2361fb226d9ee5f
Manikyam-guttula <durgamanik184@gmail.com>
2026-09-25 22:17:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ sys.path.append(os.path.dirname(os.path.abspath(__file__)))
 
 import streamlit as st
 import pandas as pd
-from src.predict import predict_attrition
+from predict import predict_attrition
 
 st.set_page_config(page_title="Employee Attrition Predictor", layout="wide")
 
